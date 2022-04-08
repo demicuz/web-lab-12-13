dbd42cc61797462090abb2bf6d82a50f59ffb3e3
demicuz <pure.demicube@gmail.com>
2022-04-09 00:16:35 +0300

    basic reactivity

--- js/index.js
+++ js/index.js
@@ -9,6 +9,7 @@ Vue.createApp({
 			rawHtml: `<span style="color: red">This should be red.</span>`,
 
 			maxImgCountString: 10,
+			imgCount: 4,
 		};
 	},
 
@@ -18,14 +19,18 @@ Vue.createApp({
 		}
 	},
 
+	// We can use computed variables like any other declared in data() (I think)
 	computed: {
 		maxImgCount() {
 			const cnt = parseInt(this.maxImgCountString);
+			return cnt && cnt > 0 ? cnt : 0;
+		},
 
-			if (cnt && cnt > 0) {
-				return cnt;
+		progressWidth() {
+			if (this.maxImgCount > 0 && this.imgCount <= this.maxImgCount) {
+				return 100 * this.imgCount / this.maxImgCount;
 			} else {
-				return 0;
+				return 100;
 			}
 		}
 	}
